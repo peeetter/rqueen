dca943f16c2ad04bd03c35fa27af54ec2e38e5d4
Pontus Lindström <pontusss85@gmail.com>
2020-07-19 20:21:04 +0200

    Router funkar via urlfönstret

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
-
 import "./App.css";
-
 import Header from "./Components/Header/Header.js";
 import Main from "./Components/Main/Main.js";
-import WhoWeAre from "./Components/WhoWeAre/WhoWeAre.js";
-import WhatWeDo from "./Components/WhatWeDo/WhatWeDo.js";
-import Contact from "./Components/Contact/Contact.js";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,35 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Header.css";
-
-import WhatWeDo from "../WhatWeDo/WhatWeDo.js";
-import WhoWeAre from "../WhoWeAre/WhoWeAre.js";
-import Contact from "../Contact/Contact.js";
 
 function Header() {
   return (
-    <Router>
-      <div className="header">
-        <ul className="navbar">
-          <li className="navbar-btn">
-            <Link to="/who-we-are" className="link">
-              Who we are
-            </Link>
-          </li>
+    <div className="header">
+      <ul className="navbar">
+        <li className="navbar-btn">
+          <Link to="/who-we-are" className="link">
+            Who we are
+          </Link>
+        </li>
 
-          <li className="navbar-btn">
-            <Link to="/what-we-do" className="link">
-              What we do
-            </Link>
-          </li>
-          <li className="navbar-btn">
-            <Link to="/contact" className="link">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </Router>
+        <li className="navbar-btn">
+          <Link to="/what-we-do" className="link">
+            What we do
+          </Link>
+        </li>
+        <li className="navbar-btn">
+          <Link to="/contact" className="link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
