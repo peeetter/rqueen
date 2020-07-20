52099aa1890d65549e0ba898b32f8ba0fc598815
Pontus Lindström <pontusss85@gmail.com>
2020-07-20 21:17:43 +0200

    Routern funkar

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Main.css";
-
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { Switch, Route } from "react-router-dom";
 import WhatWeDo from "../WhatWeDo/WhatWeDo.js";
 import WhoWeAre from "../WhoWeAre/WhoWeAre.js";
 import Contact from "../Contact/Contact.js";
@@ -11,14 +9,12 @@ import Home from "../Home/Home.js";
 function Main() {
   return (
     <div className="main">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path={`/who-we-are`} component={WhoWeAre}></Route>
-          <Route exact path="/what-we-do" component={WhatWeDo}></Route>
-          <Route exact path="/contact" component={Contact}></Route>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path={`/who-we-are`} component={WhoWeAre}></Route>
+        <Route path="/what-we-do" component={WhatWeDo}></Route>
+        <Route path="/contact" component={Contact}></Route>
+      </Switch>
     </div>
   );
 }
