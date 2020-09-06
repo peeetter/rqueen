3045af41d5f1ec37286afd177f3239eeac241778
Pontus Lindström <pontusss85@gmail.com>
2020-09-06 20:41:49 +0200

    Root is whatwedo

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,18 +1,17 @@
 import React from "react";
 import "./Main.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import WhatWeDo from "../WhatWeDo/WhatWeDo.js";
 import HowWeThink from "../HowWeThink/HowWeThink.js";
 import Contact from "../Contact/Contact.js";
-import Home from "../Home/Home.js";
 
 function Main() {
   return (
     <div className="main">
       <Switch>
-        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/" component={WhatWeDo}></Route>
         <Route exact path={`/who-we-are`} component={HowWeThink}></Route>
         <Route exact path="/what-we-do" component={WhatWeDo}></Route>
         <Route exact path="/contact" component={Contact}></Route>
