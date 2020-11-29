4146568de6237dd2e29b7dd917ee8583e4342178
Pontus Lindström <pontusss85@gmail.com>
2020-11-29 14:22:32 +0100

    Responsive layout fixes

--- src/Components/WhatWeDo/WhatWeDo.js
+++ src/Components/WhatWeDo/WhatWeDo.js
@@ -16,21 +16,25 @@ const WhatWeDo = () => {
   return (
     <div className="what-we-do">
       <div className="LeftSide">
-        <h1>
-          Tailor-made software<br></br> for your business
-        </h1>
-        <p className="WhatWeDoParagraph">
+        <h1>Tailor-made software for your business</h1>
+        <p>
           In Through the Looking-Glass by Lewis Carroll, the Red Queen is a
           character who needs to keep running in order to stay in the same
           place.
-          <br></br> Likewise, we believe that in a world which is changing more
-          rapidly than ever, companies need to constantly evolve to keep up with
-          their competition. We hold this to be as true for ourselves as for our
-          customers, and we constantly strive to make the current project more
-          amazing than the last one.
         </p>
-        <br></br>
-        <Button text="HIRE US" onClick={() => history.push("/contact")} />
+        <p>
+          Likewise, we believe that in a world which is changing more rapidly
+          than ever, companies need to constantly evolve to keep up with their
+          competition.
+        </p>
+        <p className="LastOne">
+          We hold this to be as true for ourselves as for our customers, and we
+          constantly strive to make the current project more amazing than the
+          last one.
+        </p>
+        <div className="HireUsButtonContainer">
+          <Button text="HIRE US" onClick={() => history.push("/contact")} />
+        </div>
       </div>
       <animated.div className="RightSide" style={props}>
         <SpinningLogo />
